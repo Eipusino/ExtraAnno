250da9cefb2237b90ec450bd23d9114e9b0cd400
Administrator <wdnmddd1@outlook.com>
2025-01-19 12:58:45 +0800

    update comp

--- src/lonetrail/core/LoneTrailMod.java
+++ src/lonetrail/core/LoneTrailMod.java
@@ -19,36 +19,6 @@ public class LoneTrailMod extends Mod {
     /** Lists all the mod's packages by their canonical names. Generated at compile-time. */
     public static @ListPackages Seq<String> packages = Seq.with();
 
-    public static String[] sun = {
-            "sun.misc.Unsafe"
-    };
-
-    public static String[] jdkInternal = {
-            "jdk.internal.misc.Unsafe",
-            "jdk.internal.misc.VM",
-            "jdk.internal.reflect.Reflection"
-    };
-
-    static {
-        for (String s : jdkInternal) {
-            try {
-                Class<?> a = Class.forName(s);
-                Log.info(a);
-            } catch (Throwable e) {
-                Log.err(e);
-            }
-        }
-
-        for (String s : sun) {
-            try {
-                Class<?> a = Class.forName(s);
-                Log.info(a);
-            } catch (Throwable e) {
-                Log.err(e);
-            }
-        }
-    }
-
     public LoneTrailMod() {
         Events.on(ContentInitEvent.class, e -> {
             if (!headless) {
@@ -59,9 +29,9 @@ public class LoneTrailMod extends Mod {
 
     @Override
     public void loadContent() {
-        //LUnitTypes.load();
+        LUnitTypes.load();
 
-        //EntityRegister.init();
+        EntityRegister.init();
     }
 
     @Override
